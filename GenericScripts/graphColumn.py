0bc4ee340c0e516de06d09b77ce59c859c35b98c
Luis Zaman <zamanlui@user-7b4a8f.user.msu.edu>
2011-05-18 11:43:06 -0400

    added most generic script

---
+++ GenericScripts/graphColumn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+from numpy import array
+from matplotlib import pyplot
+import gzip
+
+#usage: graphAvidaColumn.py <filename> <column>
+filename = sys.argv[1]
+column = int(sys.argv[2])
+
+#handle either .gz or non zipped files
+if filename.endswith('.gz'):
+    f = gzip.open(filename)
+else:
+    f = open(filename)
+
+#This part works only if everything is numeric in the file
+#Read in file turning them into floats
+content = f.read()
+content =  [i.strip().split() for i in content.split('\n') if len(i) > 1 and i[0] != "#"]
+content = [array(i[0:], float) for i in content]	
+toGraph = [i[column] for i in content]
+
+#graph it
+pyplot.plot(toGraph)
+pyplot.show()
