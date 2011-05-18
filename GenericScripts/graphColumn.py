333331ebe79ef4eed505177ecdfe3a95468010db
Luis Zaman <luis.zaman@gmail.com>
2011-05-18 12:07:46 -0400

    generalized a little more

--- GenericScripts/graphColumn.py
+++ GenericScripts/graphColumn.py
@@ -10,22 +10,30 @@ from numpy import array
 from matplotlib import pyplot
 import gzip
 
+#Returns an array of floats for a given column in a file 
+#assuming the columns are seperated by spaces and the rows by new lines
+#with # comment, and only numeric values (breaks with non-numeric values)
+def get_avida_column(filename, column):
+  #handle either .gz or non zipped files
+  if filename.endswith('.gz'):
+      f = gzip.open(filename)
+  else:
+      f = open(filename)
+ 
+  content = f.read()
+  content =  [i.strip().split() for i in content.split('\n') if len(i) > 1 and i[0] != "#"]
+  content = [array(i[0:], float) for i in content]
+
+  to_return = [i[column] for i in content]
+  return to_return
+
+#get command line args
 filename = sys.argv[1]
 column = int(sys.argv[2])
 
-#handle either .gz or non zipped files
-if filename.endswith('.gz'):
-    f = gzip.open(filename)
-else:
-    f = open(filename)
-
-#This part works only if everything is numeric in the file
-#Read in file turning them into floats
-content = f.read()
-content =  [i.strip().split() for i in content.split('\n') if len(i) > 1 and i[0] != "#"]
-content = [array(i[0:], float) for i in content]	
-toGraph = [i[column] for i in content]
+#get the column to graph
+to_graph = get_avida_column(filename, column)
 
 #graph it
-pyplot.plot(toGraph)
+pyplot.plot(to_graph)
 pyplot.show()
