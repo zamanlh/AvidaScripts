f9c01675d2f17a5e52e84bfe2e13c2f830d056ab
Luis Zaman <luis.zaman@gmail.com>
2011-05-18 14:34:18 -0400

    made get_avida_column work with non-numeric columns

--- GenericScripts/graphColumn.py
+++ GenericScripts/graphColumn.py
@@ -6,13 +6,12 @@
 # Luis Zaman
 # 5/18
 import sys
-from numpy import array
+from numpy import array, transpose
 from matplotlib import pyplot
 import gzip
 
-#Returns an array of floats for a given column in a file 
-#assuming the columns are seperated by spaces and the rows by new lines
-#with # comment, and only numeric values (breaks with non-numeric values)
+#Returns an array of floats for a given column in a file assuming the columns are 
+#seperated by spaces and the rows by new lines, and comments start with "#"
 def get_avida_column(filename, column):
   #handle either .gz or non zipped files
   if filename.endswith('.gz'):
@@ -21,18 +20,24 @@ def get_avida_column(filename, column):
       f = open(filename)
  
   content = f.read()
+  
+  #parse into 2d array
   content =  [i.strip().split() for i in content.split('\n') if len(i) > 1 and i[0] != "#"]
-  content = [array(i[0:], float) for i in content]
-
-  to_return = [i[column] for i in content]
-  return to_return
-
+  
+  #transpose the 2d array, so we can index the column directly
+  content_transposed = transpose(content)
+  
+  return content_transposed[column]
+  
 #get command line args
 filename = sys.argv[1]
 column = int(sys.argv[2])
 
 #get the column to graph
 to_graph = get_avida_column(filename, column)
+
+#turn it into an array of floats (new matplotlib doesn't require this step)
+to_graph = array(to_graph, float)
 
 #graph it
 pyplot.plot(to_graph)
